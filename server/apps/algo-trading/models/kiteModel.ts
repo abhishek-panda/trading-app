1dd64f162e9a49038e95ffecfb2111a5d650c332
Abhishek Panda <panda.abhis@gmail.com>
2023-04-14 12:54:37 +0530

    refactored kiteconnect class

--- server/apps/algo-trading/models/kiteModel.ts
+++ server/apps/algo-trading/models/kiteModel.ts
@@ -1,6 +1,11 @@
 const Kite = require('kiteconnect');
 import logger from '../logger';
+import { BasketOrderItem } from '../typings';
 
+
+/**
+ * TODO: Add return type of member function
+ */
 export default class KiteConnectModel {
 
     private kiteconnect: any;
@@ -20,8 +25,9 @@ export default class KiteConnectModel {
                 return response.access_token;
             })
             .catch(function(error: any) {
-                logger.error(`Failed to generate access token. ${JSON.stringify(error)}`);
-                return new Error("Failed to generate access token");
+                const errorDetails = new Error(`Failed to generate access token. ${JSON.stringify(error)}`);
+                logger.error(errorDetails.message);
+                return errorDetails;
             });
     }
 
@@ -31,84 +37,74 @@ export default class KiteConnectModel {
             .then(function(response: any) {
                return response;
             }).catch(function(error: any) {
-                logger.error(`Failed to get user profile. ${JSON.stringify(error)}`);
-                return new Error("Failed to get user profile");
+                const errorDetails = new Error(`Failed to get user profile. ${JSON.stringify(error)}`);
+                logger.error(errorDetails.message);
+                return errorDetails;
             });
     }
 
 
+    getMargin (accessToken: string , segment = "equity") {
+        this.kiteconnect.setAccessToken(accessToken);
+        return this.kiteconnect.getMargins(segment)
+            .then(function(response: any) {
+                return response;
+            }).catch(function(error: any) {
+                const errorDetails = new Error(`Failed to get margin details. ${JSON.stringify(error)}`);
+                logger.error(errorDetails.message);
+                return errorDetails;
+            });
+    }
+
+    getInstruments(accessToken: string, exchange =  'NSE') {
+        this.kiteconnect.setAccessToken(accessToken);
+        return this.kiteconnect.getInstruments(exchange).then(function(response: any) {
+                return response;
+            }).catch(function(error: any) {
+                const errorDetails = new Error(`Failed to get all instruments. ${JSON.stringify(error)}`);
+                logger.error(errorDetails.message);
+                return errorDetails;
+            })
+    }
+
+
+    getQuote(accessToken: string, instruments: string[] = []) {
+        this.kiteconnect.setAccessToken(accessToken);
+        return this.kiteconnect.getQuote(instruments).then(function(response: any) {
+                return response;
+            }).catch(function(error: any) {
+                const errorDetails = new Error(`Failed to get quote. ${JSON.stringify(error)}`);
+                logger.error(errorDetails.message);
+                return errorDetails;
+            })
+    }
+
+    getBasketMargin(accessToken: string,  basketOrders: BasketOrderItem[] = []) {
+        this.kiteconnect.setAccessToken(accessToken);
+        return this.kiteconnect.orderBasketMargins(basketOrders, true, "compact").then(function (response: any) {
+                return response;
+            }).catch(function(error: any) {
+                const errorDetails = new Error(`Failed to get basket margin. ${JSON.stringify(error)}`);
+                logger.error(errorDetails.message);
+                return errorDetails;
+            });
+    }
+
+    /*
+    * TODO: To check if basket order can be placed instead of single order
+    *  To verify the parameters.
+    */
+    placeOrder(accessToken: string, variety: string, order: BasketOrderItem) {
+        this.kiteconnect.setAccessToken(accessToken);
+        logger.info(`Placing order. ${JSON.stringify(order)}`);
+        return this.kiteconnect.placeOrder(variety, order).then(function(response: any) {
+                return response.order_id;
+            }).catch(function(error: any) {
+                const errorDetails = new Error(`Failed to place order. Order : ${JSON.stringify(order)}. Error: ${JSON.stringify(error)}`);
+                logger.error(errorDetails.message);
+                return errorDetails;
+            });
+    }
 }
 
-
-
-
-
-// function getMargin (segment = "equity") {
-//     const kiteConnection = connectKite();
-//     kiteConnection.setAccessToken(process.env.ACCESS_TOKEN);
-//     return kiteConnection.getMargins(segment)
-// 		.then(function(response: any) {
-// 			return response;
-// 		}).catch(function(error: any) {
-//             logger.error(`Failed to get margin details. ${JSON.stringify(error)}`);
-//             return '';
-// 		});
-// }
-
-// function getInstruments(exchange =  'NSE') {
-//     const kiteConnection = connectKite();
-//     kiteConnection.setAccessToken(process.env.ACCESS_TOKEN);
-// 	return kiteConnection.getInstruments(exchange).then(function(response: any) {
-//             return response;
-//         }).catch(function(error: any) {
-//             logger.error(`Failed to get all instruments. ${JSON.stringify(error)}`);
-//             return '';
-//         })
-// }
-
-// function getQuote(instruments = []) {
-//     const kiteConnection = connectKite();
-//     kiteConnection.setAccessToken(process.env.ACCESS_TOKEN);
-// 	return kiteConnection.getQuote(instruments).then(function(response: any) {
-//             return response;
-//         }).catch(function(error: any) {
-//             logger.error(`Failed to get quote. ${JSON.stringify(error)}`);
-//             return new Error('Failed to get quote');
-//         })
-// }
-
-// function getBasketMargin(basketOrders = []) {
-//     const kiteConnection = connectKite();
-//     kiteConnection.setAccessToken(process.env.ACCESS_TOKEN);
-//     return kiteConnection.orderBasketMargins(basketOrders, true, "compact").then(function (response: any) {
-//             return response;
-//         }).catch(function(error: any) {
-//             logger.error(`Failed to get basket margin. ${JSON.stringify(error)}`);
-//             return new Error('Failed to get basket margin');
-//         });
-// }
-
-// function placeOrder(variety: any, order: any) {
-//     const kiteConnection = connectKite();
-//     kiteConnection.setAccessToken(process.env.ACCESS_TOKEN);
-//     logger.info(`Placing order. ${JSON.stringify(order)}`);
-//     return kiteConnection.placeOrder(variety, order).then(function(response: any) {
-// 			return response.order_id;
-// 		}).catch(function(error: any) {
-//             logger.error(`Failed to place order. Order : ${JSON.stringify(order)}. Error: ${JSON.stringify(error)}`);
-//             return new Error('Failed to place order');
-// 		});
-// }
-
-
-
-// module.exports = {
-//     getAccessToken,
-//     getProfile,
-//     getMargin,
-//     getInstruments,
-//     getQuote,
-//     getBasketMargin,
-//     placeOrder
-// };
 // ticker.on('order_update', onTrade);
